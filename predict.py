07dea1afbc8887a0088cfc6bd1d66b02b2bdcf34
Sahil Sulekhiya <sulekhia@gmail.com>
2019-07-15 22:54:55 +0530

    Add files via upload

---
+++ predict.py
@@ -0,0 +1,35 @@
+
+import pickle
+
+
+
+import keras
+
+layers = keras.layers
+models = keras.models
+
+
+class NewsClassifier:
+    def __init__(self, model_path = "model.h5", vectorizer_path = "tokenizer.pkl", encoder_path = "encoder.pkl"):
+        self.model = keras.models.load_model(model_path)
+        self.vectorizer = pickle.load(open(vectorizer_path, 'rb'))
+        self.encoder = pickle.load(open(encoder_path, 'rb'))
+
+    def predict(self, input_string):
+        input_string = [input_string]
+        vectorized_string = self.vectorizer.texts_to_matrix(input_string)
+        prediction = self.model.predict(vectorized_string).argmax()
+        return self.encoder.inverse_transform([prediction])
+
+
+def main():
+    predictor = NewsClassifier()
+    while True:
+        ip = input()
+        if ip == "quit":
+            break
+        print(predictor.predict(ip))
+
+if __name__ == "__main__":
+    main()
+
