7c1c7480d0a9bea27b7bbac131eae53acc6ec6b7
Sahil Sulekhiya <sulekhia@gmail.com>
2019-07-17 21:10:44 +0530

    Add files via upload

--- predict.py
+++ predict.py
@@ -1,8 +1,9 @@
-import sys
+
 import numpy as np
 import pandas as pd
 import os
 import itertools
+import sys
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -17,9 +18,10 @@ from tensorflow import keras
 layers = keras.layers
 models = keras.models
 
-class Classifier:
+
+class NewsClassifier:
     def __init__(self, model_path = "model.h5", vectorizer_path = "tokenizer.pkl", encoder_path = "encoder.pkl"):
-        self.model = keras.models.load_model(model_path)
+        self.model = keras.models.load_model(model_path, compile=False)
         self.vectorizer = pickle.load(open(vectorizer_path, 'rb'))
         self.encoder = pickle.load(open(encoder_path, 'rb'))
 
@@ -27,16 +29,18 @@ class Classifier:
         input_string = [input_string]
         vectorized_string = self.vectorizer.texts_to_matrix(input_string)
         prediction = self.model.predict(vectorized_string).argmax()
+        print(prediction)
         return self.encoder.inverse_transform([prediction])
 
 
 def main():
-    predictor = Classifier()
-    inputText='code vita';
-    
-    ip=list(inputarg);
-    ip=' '.join(ip);
-    print(predictor.predict(inputText))
-    
+    predictor = NewsClassifier()
+    ip=sys.argv[1];
+    for st in ip:
+        if st==',':
+            st=' '
+    print(ip);
+    print(predictor.predict(ip))
+
 if __name__ == "__main__":
     main()
